1297f21af8e9fd8ca5d61232ba527a3edfc6f329
Patrick Einwag <patrick.einwag@allthings.me>
2018-08-31 15:04:01 +0200

    generics

---
+++ udemy/understanding-typescript/generics/solutions.js
@@ -0,0 +1,28 @@
+var MyMap = /** @class */ (function () {
+    function MyMap() {
+        this.items = {};
+    }
+    MyMap.prototype.setItem = function (key, item) {
+        this.items[key] = item;
+    };
+    MyMap.prototype.getItem = function (key) {
+        return this.items[key];
+    };
+    MyMap.prototype.clear = function () {
+        this.items = {};
+    };
+    MyMap.prototype.printMap = function () {
+        for (var key in this.items) {
+            console.log(this.items[key]);
+        }
+    };
+    return MyMap;
+}());
+var numberMap = new MyMap();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+var stringMap = new MyMap();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
---
+++ udemy/understanding-typescript/generics/solutions.ts
@@ -0,0 +1,27 @@
+class MyMap<T extends number|string> {
+    private items :{[key :string] : T} = {}
+    setItem(key :string, item :T) {
+        this.items[key] = item;
+    }
+    getItem(key :string) {
+        return this.items[key];
+    }
+    clear() {
+        this.items = {};
+    }
+    printMap() {
+        for (let key in this.items) {
+            console.log(this.items[key]);
+        }
+    }
+}
+
+const numberMap = new MyMap<number>();
+numberMap.setItem('apples', 5);
+numberMap.setItem('bananas', 10);
+numberMap.printMap();
+ 
+const stringMap = new MyMap<string>();
+stringMap.setItem('name', "Max");
+stringMap.setItem('age', "27");
+stringMap.printMap();
